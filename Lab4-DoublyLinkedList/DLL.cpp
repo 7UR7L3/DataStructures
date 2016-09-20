920bfccd5c17c2fb383e0888c84c14a230654c1b
7UR7L3 <m7ur7l3@yahoo.com>
2016-09-20 08:15:34 -0600

    Lab 4 Setup

---
+++ Lab4-DoublyLinkedList/DLL.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+//node implementation for doubly linked list
+struct Node
+{
+	int key;
+	Node *previous;
+	Node *next;
+};
+
+int main()
+{
+	cout << "lab4" << endl;
+	return 0;
+}
